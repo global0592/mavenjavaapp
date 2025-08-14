f5e76e2d82e97ba34e19cdfae104afd80db7cc5f
global0592 <dhanubhandare23@gmail.com>
2025-08-14 16:23:38 +0530

    Update App.java

--- simplejavaapp/src/main/java/com/example/App.java
+++ simplejavaapp/src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello Maven from GitHub!");
+        System.out.println("Welcome to Global IT Solutions");
     }
 }
